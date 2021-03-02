bdd513d6e1d705a1cc52f23edf84b50f017a2ed1
mirco verderosa <verderosamircowork@gmail.com>
2021-03-02 14:59:00 +0100

    Creata struttura base dell' applicazione compresa di immagini e installazione Laravel mix.

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="/dist/app.css">
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <?php
+    require_once 'database/database.php';
+     ?>
+
+
+     
+  </body>
+</html>
