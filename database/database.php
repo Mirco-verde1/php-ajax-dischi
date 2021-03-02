e97a4a30486cfec7d925371d405f25c14894ad9d
mirco verderosa <verderosamircowork@gmail.com>
2021-03-02 16:07:12 +0100

    Creata base layout e dati nella directory database

---
+++ database/database.php
@@ -0,0 +1,52 @@
+<?php
+
+$dataAlbums = [
+
+[
+  'cover'=> 'bay',
+  'albumname' => 'Chaos and the Calm',
+  'artistname' => 'James Bay',
+  'year' => '2015'
+],
+
+[
+  'cover'=> 'eminem',
+  'albumname' => 'The Marshall Mathers LP',
+  'artistname' => 'Eminem',
+  'year' => '2000'
+],
+
+[
+  'cover'=> 'fibra',
+  'albumname' => 'Fenomeno',
+  'artistname' => 'Fabri Fibra',
+  'year' => '2017'
+],
+
+[
+  'cover'=> 'franco',
+  'albumname' => 'Stanza singola',
+  'artistname' => 'Franco 126',
+  'year' => '2019'
+],
+
+[
+  'cover'=> 'jay-z',
+  'albumname' => 'Magna Carta',
+  'artistname' => 'Jay-Z',
+  'year' => '2013'
+],
+
+[
+  'cover'=> 'marra',
+  'albumname' => 'Persona',
+  'artistname' => 'Marracash',
+  'year' => '2020'
+],
+
+
+];
+
+
+
+ ?>
--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-    <link rel="stylesheet" href="/dist/app.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+      integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA=="
+      crossorigin="anonymous" />
+    <link rel="stylesheet" href="dist/app.css">
     <meta charset="utf-8">
     <title></title>
   </head>
@@ -11,7 +14,28 @@
     require_once 'database/database.php';
      ?>
 
+<div class="app-container">
+  <div class="header">
 
-     
+  </div>
+  <div class="left-blu-bar">
+
+  </div>
+
+  <div class="content-background">
+    <div class="container">
+      <div class="album-container">
+
+      </div>
+    </div>
+  </div>
+
+  <div class="right-blu-bar">
+
+  </div>
+
+</div>
+
+
   </body>
 </html>
